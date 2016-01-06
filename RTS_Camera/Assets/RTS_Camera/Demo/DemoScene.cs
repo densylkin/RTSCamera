586730cc489cef26b8437c65122a6d3d361fda2e
Denis Sylkin <densylkin@gmail.com>
2016-01-06 15:03:13 +0400

    version 1.1
    New project structure and some improvements

---
+++ RTS_Camera/Assets/RTS_Camera/Demo/DemoScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DemoScene : MonoBehaviour 
+{
+    public Button btn45;
+    public Button btn90;
+
+    private void Start()
+    {
+        Transform camT = Camera.main.transform;
+        btn45.onClick.AddListener(() => SetXRotation(camT, 45f));
+        btn90.onClick.AddListener(() => SetXRotation(camT, 90f));
+    }
+
+    private void SetXRotation(Transform t, float angle)
+    {
+        t.localEulerAngles = new Vector3(angle, t.localEulerAngles.y, t.localEulerAngles.z);
+    }
+}
